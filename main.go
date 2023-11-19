f9e92c61e27b73ad7f17c9bbdf223669cb4dd022
Rob Bridges <Rmb101187@gmail.com>
2023-11-18 16:03:24 -0800

    Selecting messages as their available

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-    countConcurrently()
+    selectChanExample()
     
 }
 
@@ -71,4 +71,31 @@ func count (thing string, c chan string) {
 		time.Sleep(time.Millisecond * 500)
 	}
 	
+}
+
+func selectChanExample() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go func() {
+		for {
+			c1 <- "Every 500ms"
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+	go func() {
+		for {
+			c2 <- "Every 2 seconds"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	for {
+		select {
+		case msg1 := <- c1:
+			fmt.Println(msg1)
+		case msg2 := <- c2:
+			fmt.Println(msg2)
+		}
+		
+	}
 }
